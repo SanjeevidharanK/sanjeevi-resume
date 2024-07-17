bf11422312438d12c68d05e275626bffa5680212
“sanjeevidharank” <sanjeevi.krishn@gmail.com>
2024-07-17 19:28:29 +0530

    [ADD]-Files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BasicContentComponent } from './basic-content/basic-content.component';
+import { AppComponent } from './app.component';
+import { HomeContentComponent } from './home-content/home-content.component';
+import { ExperienceContentComponent } from './experience-content/experience-content.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:"",component:BasicContentComponent},
+  {path:"home",component:HomeContentComponent},
+  {path:"experience",
+    loadChildren: () => import('../app/experience-content/experience-content.module').then(m => m.ExperienceContentModule)},
+    {path:"contact",component:ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BasicContentComponent } from './basic-content/basic-content.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HomeContentComponent } from './home-content/home-content.component';
+import { ExperienceContentModule } from './experience-content/experience-content.module';
+import { ContactComponent } from './contact/contact.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicContentComponent,
+    HomeContentComponent,
+    ContactComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,ReactiveFormsModule,
+    ExperienceContentModule,HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
